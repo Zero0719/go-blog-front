5fdf6c2ded12340ac5ac152c246e728274e05470
kayuHo <iamjoyhe@163.com>
2020-05-27 14:15:15 +0800

    引入iview tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="hello">
+      <div class="w-full h-10 bg-red-500"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
